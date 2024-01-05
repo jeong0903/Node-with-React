c0c95d30cd37568b3fc250288ad7c6b2ac5eac50
JeongHyeon Park <9193jane@gmail.com>
2024-01-05 14:27:33 +0900

    20240105 3 mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://JeongHyeon:dlsxjspt93!@boilerplate.ppnofzg.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요😄')
 })
